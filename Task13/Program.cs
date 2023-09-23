f4d1926f0d006a78d671c6cc07ef8aa54889050f
Николай <nnm_58@mail.ru>
2023-09-23 21:02:16 +0300

    Добавил тему и решение задачи №15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message);
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+       number /= 10;
+     
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine(Третьей цифры нет);
+        return false;
+    }
+    return trie;
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
+
+
